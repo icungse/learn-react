9980893a137a7d824cf298455267492492e6a1b6
Icungse <mzuquare@gmail.com>
2018-10-25 11:33:13 +0700

    Add simple component

---
+++ src/Mine.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Mine extends React.Component {
+    render() {
+        return(
+            <div>
+                <div id="mine">
+                    Hello World Js
+                </div>
+
+                <div>
+                    React is cool
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Mine;
