44981c273660cca6e9041fce594c0b8f019f2599
Icungse <mzuquare@gmail.com>
2018-10-25 14:30:57 +0700

    State

--- src/Mine.js
+++ src/Mine.js
@@ -2,7 +2,16 @@ import React from 'react'
 import Child from './Child';
 
 class Mine extends React.Component {
+    constructor(){
+        super()
+        this.state = {texts: ['Learning ReactJs', 'Funny', 'Cool']}
+    }
     render() {
+        let texts = this.state.texts.map(function(text) {
+            return (
+                <Child text = {text} />
+            )
+        })
         return(
             <div>
                 <div id="mine">
@@ -13,11 +22,7 @@ class Mine extends React.Component {
                     React is cool
                 </div>
 
-                <Child text="Learn"/>
-                <br/>
-                <Child text="React Js"/>
-                <br/>
-                <Child text="Cool"/>
+                {texts}
             </div>
         );
     }
